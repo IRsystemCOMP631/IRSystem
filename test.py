abf2755306e97db42b93afb385829e3d0adac51e
Kate0516 <50490358+Kate0516@users.noreply.github.com>
2022-02-19 22:36:18 -0600

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import re
-def remove_punctuation(document):
-    document = re.sub(r'[^\w\s]', '', document)
-    return document.strip()
-
-def case_folding(document):
-    return document.lower()
-
-print(remove_punctuation("Schindler's List"))
